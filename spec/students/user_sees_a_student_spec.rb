c3ecc6ca5eb42fef19bbb7d00634a561f4bdceb0
brickstar <brickstar@gmail.com>
2018-07-10 13:37:18 -0600

    user story one

--- spec/students/user_sees_a_student_spec.rb
+++ spec/students/user_sees_a_student_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe 'user visits students show' do
   it 'should see the name of a student' do
-    student = Student.create(name: 'Petey')
+    student = Student.create!(name: 'Petey')
 
-    visit student_path(student)
+    visit "/students/#{student.id}"
 
     expect(page).to have_content("Name: #{student.name}")
   end
