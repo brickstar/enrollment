35f2ef6deba31efdfdc09d0370460bd4b99c7e6c
brickstar <brickstar@gmail.com>
2018-07-10 13:55:35 -0600

    test for user can delete student

---
+++ spec/students/user_can_delete_student_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'user visits student index' do
+  it 'should be able to delete a student' do
+    student1 = Student.create!(name: 'Petey')
+    student2 = Student.create!(name: 'Matt')
+    student3 = Student.create!(name: 'Heidi')
+
+    visit "/students"
+
+    within "student-#{student2.id}" do
+      click_on 'Delete'
+    end
+
+    expect(current_path).to eq('/students')
+    expect(page).to_not have_content(student2.name)
+    expect(page).to have_content(student1.name)
+    expect(page).to have_content(student3.name)
+  end
+end
