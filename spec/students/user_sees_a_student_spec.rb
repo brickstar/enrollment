f7f24ced75b14a3d599820185da3715613936388
brickstar <brickstar@gmail.com>
2018-07-10 13:18:17 -0600

    user show spec

---
+++ spec/students/user_sees_a_student_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'user visits students show' do
+  it 'should see the name of a student' do
+    student = Student.create(name: 'Petey')
+
+    visit student_path(student)
+
+    expect(page).to have_content("Name: #{student.name}")
+  end
+end
